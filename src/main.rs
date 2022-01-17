235d052126d42eb7db851c28d4a65dd299e914f3
isofurabon <isofurabon@users.noreply.github.com>
2022-01-17 23:47:47 +0900

    chore: add license

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+//
+// MIT License
+
+// Copyright (c) 2021 rp-rs organization
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
 //! # Pico USB 'Twitchy' Mouse Example
 //!
 //! Creates a USB HID Class Poiting device (i.e. a virtual mouse) on a Pico
@@ -5,8 +29,6 @@
 //!
 //! It generates movement reports which will twitch the cursor up and down by a
 //! few pixels, several times a second.
-//!
-//! See the `Cargo.toml` file for Copyright and licence details.
 //!
 //! This is a port of
 //! https://github.com/atsamd-rs/atsamd/blob/master/boards/itsybitsy_m0/examples/twitching_usb_mouse.rs
